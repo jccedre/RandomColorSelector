a365e9c25fe983a3e20a0c270e70758df71cf723
jccedre <jccedre@gmail.com>
2014-12-04 21:22:11 -0500

    test

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var app = express();
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/public/index.html');
 }).configure(function() {
-    app.use('/', express.static(__dirname + '/public/'));
+    app.use('/', express.static(__dirname + '/assets/'));
 }).listen(port);
